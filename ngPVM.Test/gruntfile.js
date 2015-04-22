47b5d8140230b4792d0be0c8db88eb0de3dafa59
Alex Recker <alex@reckerfamily.com>
2015-04-22 10:23:23 -0500

    Test: Added test project and empty gruntfile

---
+++ ngPVM.Test/gruntfile.js
@@ -0,0 +1,7 @@
+﻿module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    grunt.registerTask('default', ['jshint', 'jasmine']);
+};
