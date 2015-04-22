9676351d9dc5a18c4c7704ef3bc86137fbc50936
Alex Recker <alex@reckerfamily.com>
2015-04-22 10:56:15 -0500

    Standards: Applied soft js linting and fixed errors
    - a few missing semicolons
    - touched up some indenting
    - adjusted newlines around bools
    
    Also added a TODO where jshint found an undeclared variable

--- ngPVM.Test/gruntfile.js
+++ ngPVM.Test/gruntfile.js
@@ -1,7 +1,32 @@
 ﻿module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            all: [
+                '../ngpvm.web/js'
+            ],
+            options: {
+                curly: true,
+                camelcase: true,
+                forin: true,
+                strict: false,
+                node: true,
+                '-W041': false,
+                globals: {
+                    '$': true,
+                    'jQuery': true,
+                    'angular': true,
+                    'commonApp': true,
+                    'currentApp': true,
+                    'moment': true,
+                    'btoa': true,
+                    'FileReader': true
+                }
+            }
+        }
     });
 
-    grunt.registerTask('default', ['jshint', 'jasmine']);
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
 };
